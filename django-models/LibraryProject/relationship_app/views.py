eb9151df2d22f892c478763dc8660d8b83282753
Chebemeze <chebemeze@gmail.com>
2025-08-04 19:11:25 +0100

    modified view.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
